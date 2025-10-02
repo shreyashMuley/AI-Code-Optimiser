179b66aa10ce8aa8474281417a99d0917c21ed16
shreyashmuley <shreyashmuley283@gmail.com>
2025-10-02 14:13:05 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require("express");//importing the express
 require("dotenv").config();
-const cors = require("cors");
+const cors = require("cors");//cors 
 const aiRoutes = require("./src/routes/ai.routes"); 
 
 const app = express();
 const PORT = 3002;
 app.use(cors()); 
-// Middleware to parse JSON (BSON TO JSON).
+// Middleware to parse JSON (BSONTO JSON).
 app.use(express.json());
 
 app.use('/ai', aiRoutes); 
